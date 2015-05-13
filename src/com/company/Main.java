e5c7aa1442b90088d3b060e02ed706c6fe5da3f4
Riki Sharma <sharma.riki@gmail.com>
2015-05-14 00:11:52 +0300

    Major bugfixes.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("The start of a beautiful game.");
-        System.out.println("Jännätesti ho,mgsma");
+        System.out.println("Jännätesti ho,mgsma!");
 
         Olio testi = new Olio(2,2);
         testi.Kerro();
